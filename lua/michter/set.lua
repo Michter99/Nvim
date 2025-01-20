ded32e5d26a0985ba69100148096dec31c589258
MiguelRichter99 <miguel.richter@klar.mx>
2025-01-20 11:28:22 -0600

    Added Relative Number

--- lua/michter/set.lua
+++ lua/michter/set.lua
@@ -1,6 +1,6 @@
 -- Always show relative line numbers
 vim.opt.number = true          -- Show the absolute number for the current line
-vim.opt.relativenumber = false -- Show relative numbers for other lines
+vim.opt.relativenumber = true  -- Show relative numbers for other lines
 
 -- Enable system clipboard
 vim.o.clipboard = 'unnamedplus'
